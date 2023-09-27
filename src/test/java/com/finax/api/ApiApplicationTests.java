bbbb5509de82b4e54b7cd774a6df811db79b9936
Alexsander Rocha <alexsanderrocha401@gmail.com>
2023-09-27 12:46:49 -0400

    Commit inicial

---
+++ src/test/java/com/finax/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finax.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
